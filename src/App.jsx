4474f86bcfc2a73310dd320239e77a0430520a9c
mamun164 <sithilove7@gmail.com>
2023-03-27 15:54:34 +0600

    header add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+import Header from './components/headers/Header'
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Header></Header>
+    </div>
+  )
+}
+
+export default App
